d44a1ac229097944ef4229f4a04697e22416111e
shivani2024 <125283312+shivani2024@users.noreply.github.com>
2023-02-13 15:06:21 +0000

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My First ever Python App')
 streamlit.header('Breakfast Menu')
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale,spinach and Rocket smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
