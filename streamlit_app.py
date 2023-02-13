d51d5dc9c06c7ca751a5b73264a6d6c525e32ff6
shivani2024 <125283312+shivani2024@users.noreply.github.com>
2023-02-13 15:01:34 +0000

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit
+import emoji
 streamlit.title('My First ever Python App')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale,spinach and Rocket smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
+streamlit.text('Avacode toast')
+streamlit.header('Create your own Fruit smoothie')
+print(emoji.emojize(":grinning_face_with_big_eyes:"))
+print(emoji.emojize(":winking_face_with_tongue:"))
+print(emoji.emojize(":zipper-mouth_face:"))
