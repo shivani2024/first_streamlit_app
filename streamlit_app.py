f07012b3aa62aac4ccfbbd26d733bfd9df2fc20f
shivani2024 <125283312+shivani2024@users.noreply.github.com>
2023-02-13 14:57:14 +0000

    Update streamlit_app.py
    Corrected the typo on line 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My First ever Python App')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlt.text('kale,spinach and Rocket smoothie')
+streamlit.text('kale,spinach and Rocket smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
