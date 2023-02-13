650a0e2913df3042403bba0b95192ca3a2854659
shivani2024 <125283312+shivani2024@users.noreply.github.com>
2023-02-13 15:14:20 +0000

    Update streamlit_app.py
    Adding Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('kale,spinach and Rocket smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
 streamlit.text('Avacode toast')
 streamlit.header('Create your own Fruit smoothie')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
