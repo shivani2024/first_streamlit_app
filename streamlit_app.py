d0e7770437fcaf0325344fd3dd12f993313e32e3
shivani2024 <125283312+shivani2024@users.noreply.github.com>
2023-02-13 14:56:32 +0000

    Update streamlit_app.py
    Added few more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My First ever Python App')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlt.text('kale,spinach and Rocket smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
