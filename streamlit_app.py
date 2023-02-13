9c006e66205f98da350d125ba7812dba34a0d3ee
shivani2024 <125283312+shivani2024@users.noreply.github.com>
2023-02-13 15:05:07 +0000

    Update streamlit_app.py
    Removed emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import emoji
 streamlit.title('My First ever Python App')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -7,6 +6,3 @@ streamlit.text('kale,spinach and Rocket smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
 streamlit.text('Avacode toast')
 streamlit.header('Create your own Fruit smoothie')
-print(emoji.emojize(":grinning_face_with_big_eyes:"))
-print(emoji.emojize(":winking_face_with_tongue:"))
-print(emoji.emojize(":zipper-mouth_face:"))
